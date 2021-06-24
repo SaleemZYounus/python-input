773807d88b64ca1a759b8687e3d4199fdf5899ff
Saleem Younus <saleemyounus@gmail.com>
2021-06-24 15:25:25 -0500

    python-input

---
+++ app.py
@@ -0,0 +1,18 @@
+name = input('input name: ')
+fav_color = input('What is your fav color? ')
+print ( 'Hi '+  name )
+print (name + ' likes ' +  fav_color)
+
+birth_year = input('birthYear: ')
+age = 2021 - int(birth_year)
+
+int() #convert string to integer
+float() #convert string to float(w/ decimal)
+bool() #convert string to boolean value
+
+
+print(age)
+
+weight_lbs = input('lbs: ')
+kilo = float(weight_lbs) * 0.45351473922
+print(kilo) 
