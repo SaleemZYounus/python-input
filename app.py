b656cf06d10ef2b3770c608d5c106c8eec0794b7
Saleem Younus <saleemyounus@gmail.com>
2021-06-24 15:43:27 -0500

    additional parameters

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 name = input('input name: ')
 fav_color = input('What is your fav color? ')
-print ( 'Hi '+  name )
-print (name + ' likes ' +  fav_color)
+print('Hi ' + name)
+print(name + ' likes ' + fav_color)
 
 birth_year = input('birthYear: ')
 age = 2021 - int(birth_year)
 
-int() #convert string to integer
-float() #convert string to float(w/ decimal)
-bool() #convert string to boolean value
-
+# int() convert string to integer
+# float() convert string to float(w/ decimal)
+# bool() convert string to boolean value
+# str() convert anything to st
 
 print(age)
 
-weight_lbs = input('lbs: ')
-kilo = float(weight_lbs) * 0.45351473922
-print(kilo) 
+weight_lbs = input('weight(lbs): ')
+weight_kg = float(weight_lbs) * 0.45351473922
+weight_kg = str(weight_kg)
+print(weight_kg)
+
+print(name + ' is ' + weight_kg + ' kgs ')
